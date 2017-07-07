ec0614fd8bcebc9c40e73b6b34de43f93146ff50
Kenny <kenny.szeto1@gmail.com>
2017-07-07 00:29:02 -0700

    Completed

--- sudoku.py
+++ sudoku.py
@@ -55,28 +55,77 @@ incorrect4 = [['a','b','c'],
 incorrect5 = [ [1, 1.5],
                [1.5, 1]]
 
-def check_sudoku():
+incorrect6 = [[1,2,4],
+             [2,4,1],
+             [4,1,2]]
+
+correct2 = [[1,2,3,4],
+            [2,4,1,3],
+            [3,1,4,2],
+            [4,3,2,1]]
+
+def check_sudoku(list):
+    columnList = []
+    index = len(list)
+    rowCount = 0
+    columnCount = 0
+    while rowCount <= index and columnCount <= index:
+        for row in list:
+            for number in row:
+                columnList.append(list[rowCount][columnCount])
+                rowCount += 1
+                if columnList == row and len(columnList) == index:
+                    columnCount += 1
+                    rowCount = 0
+                    columnList = []
+                    break
+                if columnList != row and len(columnList) == index:
+                    return False
+                if str(number).isalpha() or number != int(number):
+                    return False
+                if row.count(number) > 1:
+                    return False
+                if number + 1 not in row and number + 1 <= index:
+                    return False
+        return True
 
 
 
+# def check_sudoku(p):
+#     digit = 1
+#     length = len(p)
+#     for row in p:
+#         while digit <= length:
+#             occurs = row.count(digit)
+#             if occurs == 1:
+#                 print "occurs:" + str(occurs)
+#                 print "digits:" + str(digit)
+#                 digit += 1
+#             if occurs != 1:
+#                 print occurs
+#                 return False
+#         return True
 
-
-
-
-#print check_sudoku(incorrect)
+# print check_sudoku(incorrect)
 #>>> False
 
-#print check_sudoku(correct)
+# print check_sudoku(correct)
 #>>> True
 
-#print check_sudoku(incorrect2)
+# print check_sudoku(incorrect2)
 #>>> False
 
-#print check_sudoku(incorrect3)
+# print check_sudoku(incorrect3)
 #>>> False
 
-#print check_sudoku(incorrect4)
+# print check_sudoku(incorrect4)
 #>>> False
 
-#print check_sudoku(incorrect5)
+# print check_sudoku(incorrect5)
 #>>> False
+
+# print check_sudoku(correct2)
+#>>> True
+
+print check_sudoku(incorrect6)
+#>>> True
